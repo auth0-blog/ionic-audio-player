4f99589d881226fb48cb8812d654082b2c48bcd5
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-22 15:47:47 -0300

    done I guess;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { mediaStateReducer } from '../providers/store/store';
 import { AudioProvider } from '../providers/audio/audio';
 import { CloudProvider } from '../providers/cloud/cloud';
-import {StoreModule} from '@ngrx/store';
-import {mediaStateReducer} from '../providers/store/store';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AuthService } from '../providers/auth0/auth.service';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
     StoreModule.forRoot({
       appState: mediaStateReducer
     }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
     AudioProvider,
-    CloudProvider
+    CloudProvider,
+    AuthService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,229 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {trigger, state, style, animate, transition } from '@angular/animations';
+import {NavController, NavParams, Navbar, Content, LoadingController} from 'ionic-angular';
+import {AudioProvider} from '../../providers/audio/audio';
+import {FormControl} from '@angular/forms';
+import {CANPLAY, LOADEDMETADATA, PLAYING, TIMEUPDATE, LOADSTART, RESET} from '../../providers/store/store';
+import {Store} from '@ngrx/store';
+import {CloudProvider} from '../../providers/cloud/cloud';
+import {AuthService} from '../../providers/auth0/auth.service';
+import {pluck, filter, map, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+    trigger('showHide', [
+      state(
+        'active',
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          opacity: 0
+        })
+      ),
+      transition('inactive => active', animate('250ms ease-in')),
+      transition('active => inactive', animate('250ms ease-out'))
+    ])
+  ]
 })
 export class HomePage {
+  files: any = [];
+  seekbar: FormControl = new FormControl("seekbar");
+  state: any = {};
+  onSeekState: boolean;
+  currentFile: any = {};
+  displayFooter: string = "inactive";
+  loggedIn: Boolean;
+  @ViewChild(Navbar) navBar: Navbar;
+  @ViewChild(Content) content: Content;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public audioProvider: AudioProvider,
+    public loadingCtrl: LoadingController,
+    public cloudProvider: CloudProvider,
+    private store: Store<any>,
+    public auth: AuthService
+  ) {
+    this.auth.isLoggedIn$.subscribe((isLoggedIn: any) => {
+      this.loggedIn = isLoggedIn;
+      if (isLoggedIn) {
+        this.getDocuments();
+      }
+    });
   }
 
+  login() {
+    this.auth.login()
+      .then(() => { console.log('Successful Login'); })
+      .catch(error => { console.log(error); });
+  }
+
+  getDocuments() {
+    let loader = this.presentLoading();
+    this.cloudProvider.getFiles().subscribe(files => {
+      this.files = files;
+      loader.dismiss();
+    });
+  }
+
+  presentLoading() {
+    let loading = this.loadingCtrl.create({
+      content: 'Loading Content. Please Wait...'
+    });
+    loading.present();
+    return loading;
+  }
+
+  ionViewWillLoad() {
+    this.store.select('appState').subscribe((value: any) => {
+      this.state = value.media;
+    });
+
+    // Resize the Content Screen so that Ionic is aware of footer
+    this.store
+      .select('appState')
+      .pipe(pluck('media', 'canplay'), filter(value => value === true))
+      .subscribe(() => {
+        this.displayFooter = 'active';
+        this.content.resize();
+      });
+
+    // Updating the Seekbar based on currentTime
+    this.store
+      .select('appState')
+      .pipe(
+        pluck('media', 'timeSec'),
+        filter(value => value !== undefined),
+        map((value: any) => Number.parseInt(value)),
+        distinctUntilChanged()
+      )
+      .subscribe((value: any) => {
+        this.seekbar.setValue(value);
+      });
+  }
+
+  openFile(file, index) {
+    this.currentFile = { index, file };
+    this.playStream(file.url);
+  }
+
+  resetState() {
+    this.audioProvider.stop();
+    this.store.dispatch({ type: RESET });
+  }
+
+  playStream(url) {
+    this.resetState();
+    this.audioProvider.playStream(url).subscribe(event => {
+      const audioObj = event.target;
+
+      switch (event.type) {
+        case 'canplay':
+          return this.store.dispatch({ type: CANPLAY, payload: { value: true } });
+
+        case 'loadedmetadata':
+          return this.store.dispatch({
+            type: LOADEDMETADATA,
+            payload: {
+              value: true,
+              data: {
+                time: this.audioProvider.formatTime(
+                  audioObj.duration * 1000,
+                  'HH:mm:ss'
+                ),
+                timeSec: audioObj.duration,
+                mediaType: 'mp3'
+              }
+            }
+          });
+
+        case 'playing':
+          return this.store.dispatch({ type: PLAYING, payload: { value: true } });
+
+        case 'pause':
+          return this.store.dispatch({ type: PLAYING, payload: { value: false } });
+
+        case 'timeupdate':
+          return this.store.dispatch({
+            type: TIMEUPDATE,
+            payload: {
+              timeSec: audioObj.currentTime,
+              time: this.audioProvider.formatTime(
+                audioObj.currentTime * 1000,
+                'HH:mm:ss'
+              )
+            }
+          });
+
+        case 'loadstart':
+          return this.store.dispatch({ type: LOADSTART, payload: { value: true } });
+      }
+    });
+  }
+
+  pause() {
+    this.audioProvider.pause();
+  }
+
+  play() {
+    this.audioProvider.play();
+  }
+
+  stop() {
+    this.audioProvider.stop();
+  }
+
+  next() {
+    let index = this.currentFile.index + 1;
+    let file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  previous() {
+    let index = this.currentFile.index - 1;
+    let file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  isFirstPlaying() {
+    return this.currentFile.index === 0;
+  }
+
+  isLastPlaying() {
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  onSeekStart() {
+    this.onSeekState = this.state.playing;
+    if (this.onSeekState) {
+      this.pause();
+    }
+  }
+
+  onSeekEnd(event) {
+    if (this.onSeekState) {
+      this.audioProvider.seekTo(event.value);
+      this.play();
+    } else {
+      this.audioProvider.seekTo(event.value);
+    }
+  }
+
+  logout() {
+    this.reset();
+    this.auth.logout();
+  }
+
+  reset() {
+    this.resetState();
+    this.currentFile = {};
+    this.displayFooter = "inactive";
+  }
 }
