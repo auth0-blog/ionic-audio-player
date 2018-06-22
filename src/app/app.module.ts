b0d7f7b15814a7882a85eaa6859810868452c196
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-22 15:11:04 -0300

    audio provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AudioProvider } from '../providers/audio/audio';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AudioProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/audio/audio.ts
@@ -0,0 +1,74 @@
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import * as moment from 'moment';
+
+@Injectable()
+export class AudioProvider {
+  private stop$ = new Subject();
+  private audioObj = new Audio();
+
+  constructor() { }
+
+  private streamObservable(url) {
+    let events = [
+      'ended', 'error', 'play', 'playing', 'pause', 'timeupdate', 'canplay', 'loadedmetadata', 'loadstart'
+    ];
+
+    const addEvents = function(obj, events, handler) {
+      events.forEach(event => {
+        obj.addEventListener(event, handler);
+      });
+    };
+
+    const removeEvents = function(obj, events, handler) {
+      events.forEach(event => {
+        obj.removeEventListener(event, handler);
+      });
+    };
+
+    return Observable.create(observer => {
+      // Play audio
+      this.audioObj.src = url;
+      this.audioObj.load();
+      this.audioObj.play();
+
+      // Media Events
+      const handler = (event) => observer.next(event);
+      addEvents(this.audioObj, events, handler);
+
+      return () => {
+        // Stop Playing
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0;
+
+        // Remove EventListeners
+        removeEvents(this.audioObj, events, handler);
+      };
+    });
+  }
+
+  playStream(url) {
+    return this.streamObservable(url).pipe(takeUntil(this.stop$));
+  }
+
+  play() {
+    this.audioObj.play();
+  }
+
+  pause() {
+    this.audioObj.pause();
+  }
+
+  stop() {
+    this.stop$.next();
+  }
+
+  seekTo(seconds) {
+    this.audioObj.currentTime = seconds;
+  }
+
+  formatTime(time, format) {
+    return moment.utc(time).format(format);
+  }
+}
